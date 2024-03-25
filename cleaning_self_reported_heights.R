fe4cfe841b1e7681e4e73e1b0ba22a6d632a7119
Roz Dakin <roslyn.dakin@gmail.com>
2024-03-25 11:12:09 -0400

    describe the possible cases/formats

--- cleaning_self_reported_heights.R
+++ cleaning_self_reported_heights.R
@@ -16,8 +16,30 @@ reported_heights %>% as_tibble() # the original survey data (pre-cleaning)
 # different respondents (students) formatted/entered their height differently... we have heterogeneity in units, format/punctuation, as well as obvious errors etc 
 
 # In this script, we'll use stringr functions to clean up reported_heights
-# this is a process... we'll do as well as we can
+# the desired output is numerical data on each student's height, in consistent measurement units (let's go w/ inches), with values we know are impossible replaced w/ NA
+
+# This is a process... we'll do as well as we can!
+
 # I'm going to build it up step-by-step, and use a github repo to demo the process...
+
+reported_heights$height
+# To start, let's build something that handles the diversity of cases we get in a small subset, say, the first 150 entries.
+reported_heights$height[1:150]
+
+# Can we describe some of the heterogeneity here?
+# 1. whole (integer) numbers between XX-XX that can be assumed as human heights in inches. what XX-XX? 48-84 seems reasonable (4 feet - 7 feet)
+# 2. whole (integer) numbers that are between 4-7, that we assume are heights given in feet
+  # 2b. decimal numbers between 4-7, that we assume are `feet.inches`
+  # 2c. numbers with an intervening space, and the first one is is an integter between 4-7, that we assume are `feet inches`
+# 3. decimal numbers that are between 48-84, that we assume are given in inches
+# 4. whole (integer) or decimal numbers that are between 120ish - 210ish, that we assume are heights in cm
+# 5. really big numbers that we assume are errors
+# 6. numbers with ' separator that can be converted to feet and inches
+  # 6b. numbers with , separator that can be converted to feet and inches
+# ...
+
+reported_heights[1:150,]
+
 
 
 
